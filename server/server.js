adb8f54011ca2321e2f7504e111a20f6cc5e0a7c
Max Solomonyuk <dev@Maxs-MacBook-Pro.local>
2018-12-17 13:36:56 -0500

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -29,6 +30,26 @@ app.get('/todos', (req, res) => {
       res.status(400).send(e);
     }
   );
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  // if the id is not valid
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  // if id is valid
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch(e => {
+      res.send(400).send();
+    });
 });
 
 app.listen(3000, () => {
