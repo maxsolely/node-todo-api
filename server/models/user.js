3524ce5e2bde8fe1eb4298777baa12746bde4872
Max Solomonyuk <dev@Maxs-MacBook-Pro.local>
2018-12-21 09:39:37 -0500

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -32,6 +33,21 @@ var UserSchema = new mongoose.Schema({
       }
     }
   ]
+});
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 //UserSchema.methods is a function for instances (users) UserScema.statics is a function on the model itself (User)
