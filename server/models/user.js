454bb706063c94531f714c6a2e1bc7daefed25e8
Max Solomonyuk <dev@Maxs-MBP.home>
2018-12-23 10:04:10 -0500

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -35,21 +35,6 @@ var UserSchema = new mongoose.Schema({
   ]
 });
 
-UserSchema.pre('save', function(next) {
-  var user = this;
-
-  if (user.isModified('password')) {
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(user.password, salt, (err, hash) => {
-        user.password = hash;
-        next();
-      });
-    });
-  } else {
-    next();
-  }
-});
-
 //UserSchema.methods is a function for instances (users) UserScema.statics is a function on the model itself (User)
 
 // I believe toJSON is a function that gets called in the background when you res.send(user) from a successful API call
@@ -93,6 +78,46 @@ UserSchema.statics.findByToken = function(token) {
     'tokens.access': 'auth'
   });
 };
+
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this;
+
+  return User.findOne({ email }).then(user => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    // bcrypt.compare(password, user.password).then(res => {
+    //   if (res) {
+    //     return user;
+    //   }
+    // });
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserSchema);
 
